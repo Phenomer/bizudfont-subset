381329b935cf691cb15ea68ab419ab0c06fb9077
Akito Miura <phenomer@g.hachune.net>
2022-03-23 23:51:32 +0900

    とりあえず動いた

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # bizudfont-subset
-BIZ UDフォントのサブセットを作るスクリプト
+BIZ UDフォントのWOFF2形式サブセットを作るスクリプト
+
+## つかいかた
+pipenvとcurl、unzipが必要です。
+
+```Shell
+$ pipenv install
+$ ./build.sh
+```
+
+変換に成功すると、fontsディレクトリ以下にwoff2ファイルが出力されます。
+```Shell
+$ du -sk fonts/*
+896     fonts/BIZUDGothic-Bold.woff2
+888     fonts/BIZUDGothic-Regular.woff2
+1164    fonts/BIZUDMincho-Regular.woff2
+888     fonts/BIZUDPGothic-Bold.woff2
+880     fonts/BIZUDPGothic-Regular.woff2
+1160    fonts/BIZUDPMincho-Regular.woff2
+```
+
+## フォントに含める文字を変更
+jis1.txtを編集してください。
+デフォルトではJIS第一水準の漢字までが含まれているはずです。
+
+## 参考
+- [Wikipedia - JIS X 0213漢字一覧の1面](https://ja.wikipedia.org/wiki/JIS_X_0213%E6%BC%A2%E5%AD%97%E4%B8%80%E8%A6%A7%E3%81%AE1%E9%9D%A2)
+- [Wikipedia - JIS X 0213非漢字一覧](https://ja.wikipedia.org/wiki/JIS_X_0213%E9%9D%9E%E6%BC%A2%E5%AD%97%E4%B8%80%E8%A6%A7)
---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+
+mkdir -p org
+
+(cd org
+  if [ ! -e morisawa-biz-ud-gothic-fonts.zip ]; then
+      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-gothic/releases/download/v1.0.0-correctedBuild/morisawa-biz-ud-gothic-fonts.zip
+  fi
+
+  if [ ! -e morisawa-biz-ud-mincho-fonts.zip ]; then
+      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-mincho/releases/download/v1.0.0-correctedBuild/morisawa-biz-ud-mincho-fonts.zip
+  fi
+  unzip -o morisawa-biz-ud-gothic-fonts.zip
+  unzip -o morisawa-biz-ud-mincho-fonts.zip
+)
+
+mkdir -p fonts
+for TTF in org/morisawa-biz-ud-*-fonts/fonts/ttf/*.ttf; do
+    pipenv run pyftsubset ${TTF} \
+	       --text-file=jis1.txt \
+	       --layout-features='*' \
+	       --flavor=woff2 \
+	       --output-file=fonts/`basename ${TTF} .ttf`.woff2
+done
