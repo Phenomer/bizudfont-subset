c37931eb1c8383bd644c8cbf18973e40187e2b10
Akito Miura <phenomer@g.hachune.net>
2024-10-08 18:31:09 +0900

    ソースとなるフォントを最新バージョンに更新

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -x
 
 mkdir -p org
 
-(cd org
+(cd org || exit
   if [ ! -e morisawa-biz-ud-gothic-fonts.zip ]; then
-      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-gothic/releases/download/v1.002/morisawa-biz-ud-gothic-fonts.zip
+      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-gothic/releases/download/v1.051/morisawa-biz-ud-gothic-fonts.zip
   fi
 
   if [ ! -e morisawa-biz-ud-mincho-fonts.zip ]; then
-      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-mincho/releases/download/v1.002/morisawa-biz-ud-mincho-fonts.zip
+      curl -O -L https://github.com/googlefonts/morisawa-biz-ud-mincho/releases/download/v1.06/morisawa-biz-ud-mincho-fonts.zip
   fi
   unzip -o morisawa-biz-ud-gothic-fonts.zip
   unzip -o morisawa-biz-ud-mincho-fonts.zip
@@ -18,9 +18,9 @@ mkdir -p org
 
 mkdir -p fonts
 for TTF in org/morisawa-biz-ud-*-fonts/fonts/ttf/*.ttf; do
-    pipenv run pyftsubset ${TTF} \
+    uv run pyftsubset "${TTF}" \
 	       --text-file=jis1.txt \
 	       --layout-features='*' \
 	       --flavor=woff2 \
-	       --output-file=fonts/`basename ${TTF} .ttf`.woff2
+	       --output-file="fonts/$(basename "${TTF}" .ttf)-subset.woff2"
 done
